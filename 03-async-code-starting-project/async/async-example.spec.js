91800703d2b0a97fef66f94b27f006859f745119
Enkindu Weles <enkindu.weles@st-one.io>
2023-09-04 10:58:09 -0300

    Add: async test with callback, promises and async/await

---
+++ 03-async-code-starting-project/async/async-example.spec.js
@@ -0,0 +1,28 @@
+import { it, expect } from "vitest";
+import { generateToken, generateTokenPromise } from "./async-example";
+
+it("should generate a token value (callback)", (done) => {
+  const testUserEmail = "test@test.com";
+
+  generateToken(testUserEmail, (err, token) => {
+    try {
+      expect(token).toBeDefined();
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
+});
+
+it("should generate a token value (promise)", () => {
+  const testUserEmail = "test@test.com";
+
+  return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+});
+
+it("should generate a token value (async/await)", async () => {
+  const testUserEmail = "test@test.com";
+
+  const token = await generateTokenPromise(testUserEmail);
+  expect(token).toBeDefined();
+});
